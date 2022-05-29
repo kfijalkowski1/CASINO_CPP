939ea06d6c67245ef8b77d1a57e453cc0c22cfcb
kfijalk1 <k.1.fijalkowski@gmail.com>
2022-05-29 23:21:53 +0200

    implemented first Craps, internal logic, no connection to interface

---
+++ src/GamesManager/Craps/Craps.cpp
@@ -0,0 +1,64 @@
+#include "Craps.h"
+
+CrPlayer::CrPlayer(Player player)
+{
+    name = player.name;
+    id = player.id;
+    password = player.password;
+    cash = player.cash;
+}
+
+void CrPlayer::setBets(unsigned int bet, unsigned int betType, unsigned int bet1, unsigned int bet2 = 0)
+{
+    this->bet = bet;
+    this->betType = betType;
+    this->bet1 = bet1;
+    this->bet2 = bet2;
+}
+Score CrPlayer::giveDices(unsigned int dice1, unsigned int dice2)
+{
+    Score resultScore;
+    resultScore.gameId = 444;
+    resultScore.playerId = this->id;
+    if (betType == 1)
+    {
+        if ((dice1 + dice2) == bet1)
+        {
+            resultScore.score = (bet * 2);
+        }
+    }
+    else if (betType == 2)
+    {
+        if ((dice1 == bet1) && (dice2 == bet1))
+        {
+            resultScore.score = (bet * 3);
+        }
+    }
+    else if (betType == 3)
+    {
+        if ((dice1 == bet1) && (dice2 == bet2))
+        {
+            resultScore.score = (bet * 4);
+        }
+    }
+    this->cash += resultScore.score;
+    return resultScore;
+}
+
+void Craps::addPlayer(Player &newPlayer)
+{
+    CrPlayer newCrplayer(newPlayer);
+    players.push_back(newCrplayer);
+}
+
+void Craps::startNewDeal()
+{
+    // rolls dices and asserts if someone won based on PAST SETBETS
+    // rly imporatnt, first setBets, then use startNewDeal
+    dices.roll();
+    for (auto player : players)
+    {
+        Score tempScore = player.giveDices(dices.dice1, dices.dice2);
+        //@TODO somehow send this tempScore to Score board
+    }
+}
--- src/GamesManager/Craps/Craps.h
+++ src/GamesManager/Craps/Craps.h
@@ -1,2 +1,53 @@
 // Craps is a dice game in which players bet on the outcomes of a pair of dice
 // Players can wager money against each other.
+// the rules are:
+// you can bet on 3 diff things:
+// sum, pair(eg. 2, 2), specific(eg. 1, 2)
+
+#pragma once
+#include "../Cards/Cards.h"
+#include "../Game/Game.h"
+class Dices
+{
+    // there are always two dices and they are just 2 radnom num in 0-6 range
+public:
+    unsigned int dice1 = rand() % 6;
+    unsigned int dice2 = rand() % 6;
+
+    Dices(){};
+    void roll()
+    {
+        dice1 = rand() % 6;
+        dice2 = rand() % 6;
+    }
+};
+class CrPlayer : public Player
+{
+public:
+    CrPlayer();
+    CrPlayer(Player player);
+    unsigned int bet = 0;
+    unsigned int betType = 1; // 1 -> sum, 2 -> pair, 3 -> specific, used for mux of return if win
+    unsigned int bet1 = 0;    // used for specific player bets eg. if sum -> bet1 is sum, if pair -> bet1 is what of
+    unsigned int bet2 = 0;
+    void setBets(unsigned int bet, unsigned int betType, unsigned int bet1, unsigned int bet2 = 0);
+    Score giveDices(unsigned int dice1, unsigned int dice2);
+};
+
+class Craps : public Game
+{
+private:
+    unsigned int gameId = 444;
+    Dices dices;
+    std::vector<CrPlayer> players;
+
+public:
+    Craps(std::vector<CrPlayer> players)
+    {
+        this->players = players;
+    }
+    Craps(){};
+    void addPlayer(Player &player);
+    void summarise();
+    void startNewDeal();
+};
