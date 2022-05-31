f93d860633b07eaae26cd7c6aed730cc4ed6766a
kfijalk1 <k.1.fijalkowski@gmail.com>
2022-05-31 21:48:21 +0200

    copied from main for it to compile

--- src/GamesManager/BlackJack/BlackJack.h
+++ src/GamesManager/BlackJack/BlackJack.h
@@ -14,43 +14,41 @@
 
 class BJHand
 {
-  public:
-    BJHand();
-    bool isSplitted = false;
-    // std::vector<int> points = {0, 0};
-    int getPoints(bool Splitted);
-    std::vector<std::vector<Card>> hands = {{}, {}};
-    void addCard(Card card);
-    void addCard(Card card, bool toSplitted);
-    void reset();
+public:
+  BJHand();
+  bool isSplitted = false;
+  // std::vector<int> points = {0, 0};
+  int getPoints(bool Splitted);
+  std::vector<std::vector<Card>> hands = {{}, {}};
+  void addCard(Card card);
+  void addCard(Card card, bool toSplitted);
+  void reset();
 };
 class BJPlayer : public Player
 {
-  public:
-    BJPlayer();
-    BJPlayer(Player player);
-    BJHand hand;
-    unsigned int bet = 0;
+public:
+  // BJPlayer(Player player);
+  BJHand hand;
+  unsigned int bet = 0;
 };
 
 class BlackJack : public Game
 {
-  private:
-    std::vector<BJPlayer> playerBuffor = {};
-    std::vector<BJPlayer> players;
-    BJPlayer dealer;
-    Deck deck;
-    int getBet();
+private:
+  std::vector<BJPlayer> playerBuffor = {};
+  std::vector<BJPlayer> players;
+  BJPlayer dealer;
+  Deck deck;
+  // int getBet();
 
-  public:
-    BlackJack(unsigned int nOfStdDecks);
-    void startNewDeal();
-    void addPlayer(Player &player);
-    void
-    initalDistribiution(); // Distribute one card to each player and take bets
-    void makeMove(Player &player, char decision);
-    void summarise(); // each player: 1.update money and bets 2.Write Score to
-                      // buf 3.reset hands
-    void draw(); // draws current state of game (usually after each move and at
-                 // the beggining)
+public:
+  BlackJack(unsigned int nOfStdDecks);
+  void startNewDeal();
+  void addPlayer(Player &player);
+  // void initalDistribiution(); // Distribute one card to each player and take bets
+  void makeMove(Player &player, char decision);
+  // void summarise(); // each player: 1.update money and bets 2.Write Score to
+  //  buf 3.reset hands
+  // void draw();      // draws current state of game (usually after each move and at
+  //  the beggining)
 };
